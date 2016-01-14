05789f2494b8d37bb20f02f82e15b2eff3fffa59
Jason Frame <jason@onehackoranother.com>
2016-01-14 10:54:29 +0000

    example

--- main.swift
+++ main.swift
@@ -1,3 +1,13 @@
 import SDL2_swift
 
+Init()
+
+let win = Window()
+
+win.showMessageBoxWithTitle("Hello", message: "This is a message")
+
+//let win = Window(title: "Hello from Swift", width: 400, height: 300)
+//win.resizeToWidth(1024, height: 768)
+//win.hide()
+
 createWindowAndWait()
