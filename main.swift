cb243b07179781df086a9795d97d31061d1e9818
Jason Frame <jason@onehackoranother.com>
2016-02-13 22:08:36 +0000

    working!

--- main.swift
+++ main.swift
@@ -1,13 +1,20 @@
 import spline
 
 class Sketch : Sketch2D {
-	override func setup() {
-		fps(1)
-		size(width: 800, height: 600)
+	var x: Double = 0
+
+	init() {
+		super.init(width: 300, height: 200)
 	}
 
 	override func loop(clock: Clock) {
-		print("update!")
+		x = (x + 1) % 300
+		clear(r: 0.0, g: 0.0, b: 1.0)
+		stroke(r: 0.0, g: 1.0, b: 0.0)
+		begin()
+		moveTo(x: x, y: 100)
+		lineTo(x: x+5, y: 100)
+		stroke()
 	}
 }
 
