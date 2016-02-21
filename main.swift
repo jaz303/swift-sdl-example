7f8f65a308cd52fbf9dde27b417710cc8c4f13ea
Jason Frame <jason@onehackoranother.com>
2016-02-21 00:30:43 +0000

    use matrix ops in demo program

--- main.swift
+++ main.swift
@@ -12,8 +12,11 @@ class Sketch : Sketch2D {
 		clear(r: 0.0, g: 0.0, b: 1.0)
 		stroke(r: 0.0, g: 1.0, b: 0.0)
 		begin()
-		moveTo(x: x, y: 100)
-		lineTo(x: x+5, y: 100)
+		pushMatrix()
+		translate(dx: x, dy: 0)
+		moveTo(x: 0, y: 100)
+		lineTo(x: 5, y: 100)
+		popMatrix()
 		stroke()
 	}
 }
